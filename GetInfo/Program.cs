9af8a18c0ba5b42aef23676c065c69a7f456d14e
Antonio Yves <yvissousa@gmail.com>
2022-06-11 21:17:54 -0300

    Adicionar arquivos de projeto.

---
+++ GetInfo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Management;
+using Microsoft.VisualBasic.Devices;
+
+namespace GetInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerInfo computerInfo = new ComputerInfo();
+
+            Console.WriteLine("========== Informações do Sistema ==========\n");
+            Console.WriteLine("SO: " + computerInfo.OSFullName);
+            Console.WriteLine("Memória RAM: " + Math.Round(computerInfo.TotalPhysicalMemory / Math.Pow(1024.0, 3.0)) + "GB");
+            Console.WriteLine("\n============================================\n");
+
+            ManagementClass mc_proc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc_proc = mc_proc.GetInstances();
+
+            Console.WriteLine("========== Processador(es) ==========\n");
+
+            foreach (ManagementObject mo in moc_proc)
+            {
+                Console.WriteLine("Processador: " + mo.Properties["name"].Value.ToString());
+                String arch = mo.Properties["Architecture"].Value.ToString();
+                if (arch.Equals("0"))
+                {
+                    Console.WriteLine("Arquitetura: x86");
+                } else if (arch.Equals("1"))
+                {
+                    Console.WriteLine("Arquitetura: MIPS");
+                } else if (arch.Equals("2"))
+                {
+                    Console.WriteLine("Arquitetura: Alpha");
+                } else if (arch.Equals("3"))
+                {
+                    Console.WriteLine("Arquitetura: PowerPC");
+                } else if (arch.Equals("5"))
+                {
+                    Console.WriteLine("Arquitetura: ARM");
+                } else if (arch.Equals("6"))
+                {
+                    Console.WriteLine("Arquitetura: ia64");
+                } else if (arch.Equals("9"))
+                {
+                    Console.WriteLine("Arquitetura: x64");
+                } else if (arch.Equals("12"))
+                {
+                    Console.WriteLine("Arquitetura: ARM64");
+                } else
+                {
+                    Console.WriteLine("Arquitetura: desconhecida");
+                }
+            }
+            Console.WriteLine("\n=====================================\n");
+
+            ManagementClass mc_disk = new ManagementClass("Win32_LogicalDisk");
+            ManagementObjectCollection moc_disk = mc_disk.GetInstances();
+
+            Console.WriteLine("========== Disco(s) ==========\n");
+
+            foreach (ManagementObject mo in moc_disk)
+            {
+                Console.WriteLine("Letra da Unidade: " + mo.Properties["name"].Value.ToString());
+                Console.WriteLine("Nome da Unidade: " + mo.Properties["VolumeName"].Value.ToString());
+                Console.WriteLine("Capacidade: " + Math.Round(Int64.Parse(mo.Properties["size"].Value.ToString()) / Math.Pow(1024.0, 3.0)) + "GB");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("==============================\n");
+
+            ManagementClass mc_net = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc_net = mc_net.GetInstances();
+
+            Console.WriteLine("========== Adaptador(es) de Rede ==========\n");
+
+            foreach (ManagementObject mo in moc_net)
+            {
+                if (mo.Properties["IPEnabled"].Value.ToString().Equals("True"))
+                {
+                    Console.WriteLine("Nome do Adaptador de Rede: {0}", mo.Properties["Description"].Value.ToString());
+                    Console.WriteLine("Endereço MAC: {0}", mo.Properties["MACAddress"].Value.ToString());
+                    try
+                    {
+                        string[] ips = (string[])mo.Properties["IPAddress"].Value;
+                        foreach (string ip in ips)
+                        {
+                            if (ip.Contains("."))
+                            {
+                                Console.WriteLine("Endereço de IPV4: " + ip);
+                            } else
+                            {
+                                Console.WriteLine("Endereço de IPV6: " + ip);
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    catch (NullReferenceException)
+                    {
+                        Console.WriteLine("Não foi possível recuperar o endereço de IP");
+                    }
+                }
+            }
+            
+            Console.WriteLine("===========================================");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GetInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Informações Técnicas")]
+[assembly: AssemblyDescription("Com esse aplicativo é possível obter algumas informações técnicas do seu dispositivo.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Antonio Yves")]
+[assembly: AssemblyProduct("Informações Técnicas")]
+[assembly: AssemblyCopyright("Copyright © Antonio Yves 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b2710c9a-e06d-4f76-bbee-21de0e4a5156")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
